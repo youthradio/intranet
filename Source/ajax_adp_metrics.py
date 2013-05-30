6ff3eb46471bf0c3737ce6354b3ec9ba39bfc8a9
Kurt Collins <kurt@discolorations.com>
2013-05-29 17:44:45 -0700

    Modularization

--- Source/ajax_adp_metrics.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, g, request, render_template, jsonify
-from pytz import timezone
-
-import datetime, time, pytz
-import urllib2, urllib, json
-
-from util_metrics import *
-
-
